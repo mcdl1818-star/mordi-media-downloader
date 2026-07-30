8bfa1bd334a5c78492d855ce5a5fadf17fbe6c73
ml054 <ml0548526196@gmail.com>
2026-07-31 02:32:28 +0300

    Harden cloud monitor onboarding

--- monitor-bot/start-monitor.cmd
+++ monitor-bot/start-monitor.cmd
@@ -1,3 +1,3 @@
 @echo off
-cd /d "C:\Users\ml054\Documents\הורדה מיוטיוב\monitor-bot"
-"C:\Program Files\nodejs\node.exe" src\index.js >> monitor.out.log 2>> monitor.err.log
+rem הבוט עבר ל-Render ואינו מופעל עוד מהמחשב המקומי.
+exit /b 0
